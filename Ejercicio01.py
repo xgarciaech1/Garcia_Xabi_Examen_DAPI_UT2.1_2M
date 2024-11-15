d322643d37bbee2981b9f6d35440e6ec928d7b04
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-11-15 09:40:53 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,10 @@
+comida = input('Dime tu comida favorita: ')
+
+def me_encanta(comida):
+    print('me encanta', comida, 'tambien')
+    return
+me_encanta(comida)
+
+edad = input('dime tu edad en años: ')
+vivido = int(edad * 12)
+print('Has vivido aproximadamente', vivido, 'meses')
