d004685f0c169b304ce83582095613e471a5de3e
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-11-15 09:40:57 +0100

    Create Ejercicio02.py

---
+++ Ejercicio02.py
@@ -0,0 +1,11 @@
+nombre_completo = input('Dime tu nombre completo: ')
+
+
+palabra = input('Dime una palabra: ')
+if a in palabra:
+    print(vocal.upper(palabra))
+
+palabra = input('Dime una palabra: ')
+vocal = input('dime una vocal: ')
+vocal.upper()
+print()
