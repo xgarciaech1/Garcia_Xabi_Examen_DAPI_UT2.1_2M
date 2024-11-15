c4fcb3668c8d976de7053941982ee1ec2a50e5f6
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-11-15 09:41:00 +0100

    Create Ejercicio03.py

---
+++ Ejercicio03.py
@@ -0,0 +1,10 @@
+positivo = input('Dime un numero positivo: ')
+n = (0 + 2)
+if positivo == n:
+    print('es par')
+else:
+    print('es inpar')
+numero_entero = input('Dime un numero entero: ')
+lista =[]
+for i in range(len(lista)):
+    
